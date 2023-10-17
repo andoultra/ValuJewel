b726c8b6939c4486a9b239e161d06b51b0e68bd5
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-17 11:57:08 -0500

    new_function

--- netlify/functions/generate-description.js
+++ netlify/functions/generate-description.js
@@ -5,13 +5,18 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 async function describe_jewelry(type, material) {
   try {
-    const response = await openai.createCompletion({
-      engine: 'davinci-codex',
-      prompt: `Describe a jewelry piece with the following attributes: Type: ${type}, Material: ${material}`,
-      max_tokens: 150
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{
+        role: 'system',
+        content: 'You are a helpful assistant.'
+      }, {
+        role: 'user',
+        content: `Describe a jewelry piece with the following attributes: Type: ${type}, Material: ${material}`
+      }]
     });
     
-    const description = response.data.choices[0].text.trim();
+    const description = response.data.choices[0].message.content.trim();
     console.log(description);
   } catch (error) {
     console.error('Error:', error.response ? error.response.data : error.message);
