b1de6d10e89336e8b3cfe150752330fe6efb6f3a
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-17 14:51:39 -0500

    smallchanges

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-// script.js
+const jewelryForm = document.getElementById('jewelryForm');
+const descriptionElement = document.getElementById('description');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const jewelryForm = document.getElementById('jewelryForm');
-    const descriptionElement = document.getElementById('description');
+jewelryForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-    jewelryForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
+    const jewelryType = document.getElementById('jewelryType').value;
+    const jewelryMaterial = document.getElementById('jewelryMaterial').value;
+    const Cut = document.getElementById('Cut').value; // Capture Cut value
 
-        const jewelryType = document.getElementById('jewelryType').value;
-        const jewelryMaterial = document.getElementById('jewelryMaterial').value;
+    try {
+        const response = await fetch('/.netlify/functions/generate-description', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ jewelryType, jewelryMaterial, Cut }), // Include Cut in the API request
+        });
 
-        try {
-            const response = await fetch('https://willowy-pie-2fe033.netlify.app/.netlify/functions/generate-description', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ jewelryType, jewelryMaterial }),
-            });
-
-            if (!response.ok) {
-                throw new Error('Failed to fetch description');
-            }
-
-            const data = await response.json();
-            descriptionElement.textContent = data.description;
-        } catch (error) {
-            console.error('Error:', error);
+        if (!response.ok) {
+            throw new Error('Failed to fetch description');
         }
-    });
+
+        const data = await response.json();
+        descriptionElement.textContent = data.description;
+    } catch (error) {
+        console.error('Error:', error);
+    }
 });
