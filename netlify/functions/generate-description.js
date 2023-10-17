8df577269be6cba6c4b721b1ba9f526dc6746f93
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-17 12:10:13 -0500

    new

--- netlify/functions/generate-description.js
+++ netlify/functions/generate-description.js
@@ -1,4 +1,6 @@
 const OpenAI = require('openai');
+
+// Ensure to replace 'your_api_key_here' with your actual API key
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 async function describe_jewelry(type, material) {
@@ -14,11 +16,17 @@ async function describe_jewelry(type, material) {
       }]
     });
     
-    const description = completion.data.choices[0].message.content.trim();;
+    const description = completion.data.choices[0].message.content.trim();
     console.log(description);
   } catch (error) {
     console.error('Error:', error.response ? error.response.data : error.message);
   }
 }
 
-describe_jewelry('Ring', 'Gold');
+exports.handler = async (event) => {
+    await describe_jewelry('Ring', 'Gold');
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Description generated'),
+    };
+};
