d38f4f7be33015892a207a6a522517448a581250
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-17 12:23:23 -0500

    restart

--- netlify/functions/generate-description.js
+++ netlify/functions/generate-description.js
@@ -1,28 +1,28 @@
-const OpenAI = require('openai');
+const axios = require('axios');
 
-// Ensure to replace 'your_api_key_here' with your actual API key
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const apiKey = process.env.OPENAI_API_KEY;
 
 async function describe_jewelry(type, material) {
   try {
-    const completion = await openai.completions.create({
-      messages: [
-        { role: 'user', content: `Describe a jewelry piece with the following attributes: Type: ${type}, Material: ${material}` }
-      ],
-      model: 'gpt-3.5-turbo'
-    });
-    
-    const description = completion.data.choices[0].message.content.trim();
+    const response = await axios.post(
+      'https://api.openai.com/v1/engines/davinci-codex/completions',
+      {
+        prompt: `Describe a jewelry piece with the following attributes: Type: ${type}, Material: ${material}`,
+        max_tokens: 150,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+      }
+    );
+
+    const description = response.data.choices[0].text.trim();
     console.log(description);
   } catch (error) {
     console.error('Error:', error.response ? error.response.data : error.message);
   }
 }
 
-exports.handler = async (event) => {
-  await describe_jewelry('Ring', 'Gold');
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Description generated'),
-  };
-};
+describe_jewelry('Ring', 'Gold');
