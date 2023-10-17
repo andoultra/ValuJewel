c6a695bb4b8868b654a75eb07248b015eefe2bec
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-17 11:55:17 -0500

    openai

--- netlify/functions/generate-description.js
+++ netlify/functions/generate-description.js
@@ -1,26 +1,21 @@
 const OpenAI = require('openai');
 
+// Ensure to replace 'your_api_key_here' with your actual API key
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
-exports.handler = async function(event, context) {
+async function describe_jewelry(type, material) {
   try {
-    const { type, material } = JSON.parse(event.body);
-    const response = await openai.completions({
+    const response = await openai.createCompletion({
       engine: 'davinci-codex',
       prompt: `Describe a jewelry piece with the following attributes: Type: ${type}, Material: ${material}`,
       max_tokens: 150
     });
     
     const description = response.data.choices[0].text.trim();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ description })
-    };
+    console.log(description);
   } catch (error) {
     console.error('Error:', error.response ? error.response.data : error.message);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' })
-    };
   }
-};
+}
+
+describe_jewelry('Ring', 'Gold');
