53547dca6cc2cf181aad2966ea648d97963716a3
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-17 11:45:11 -0500

    new_npm

--- netlify/functions/generate-description.js
+++ netlify/functions/generate-description.js
@@ -1,26 +1,20 @@
-const axios = require('axios');
+const OpenAI = require('openai');
 
-exports.handler = async function(event, context) {
-  const jewelryData = JSON.parse(event.body);
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
+async function describe_jewelry(type, material) {
   try {
-    const response = await axios.post('https://api.openai.com/v1/engines/gpt-3.5-turbo/completions', {
-      prompt: `Describe a jewelry piece with the following attributes: Type: ${jewelryData.type}, Material: ${jewelryData.material}`,
+    const response = await openai.complete({
+      engine: 'davinci-codex',
+      prompt: `Describe a jewelry piece with the following attributes: Type: ${type}, Material: ${material}`,
       max_tokens: 150
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-      }
     });
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ description: response.data.choices[0].text.trim() })
-    };
+    
+    const description = response.data.choices[0].text.trim();
+    console.log(description);
   } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' })
-    };
+    console.error('Error:', error.response ? error.response.data : error.message);
   }
-};
+}
+
+describe_jewelry('Ring', 'Gold');
