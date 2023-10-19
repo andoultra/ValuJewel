605a844601eedc703cfca50eb3d7b259fca34d3a
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-19 06:19:58 -0500

    changed_to_get

--- script.js
+++ script.js
@@ -4,20 +4,14 @@ const descriptionElement = document.getElementById('description');
 jewelryForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const jewelryType = document.getElementById('jewelryType').value;
+    const jewelryType = encodeURIComponent(document.getElementById('jewelryType').value);
+    const jewelryMaterial = encodeURIComponent(document.getElementById('jewelryMaterial').value);
+    const Cut = encodeURIComponent(document.getElementById('Cut').value); // Capture Cut value
 
-    const jewelryMaterial = document.getElementById('jewelryMaterial').value;
-
-    const Cut = document.getElementById('Cut').value; // Capture Cut value
+    const requestURL = `https://willowy-pie-2fe033.netlify.app/.netlify/functions/generate-description?type=${jewelryType}&material=${jewelryMaterial}&Cut=${Cut}`;
 
     try {
-        const response = await fetch('https://willowy-pie-2fe033.netlify.app/.netlify/functions/generate-description', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ jewelryType, jewelryMaterial, Cut }), // Include Cut in the API request
-        });
+        const response = await fetch(requestURL);
 
         if (!response.ok) {
             throw new Error('Failed to fetch description');
@@ -27,5 +21,6 @@ jewelryForm.addEventListener('submit', async (event) => {
         descriptionElement.textContent = data.description;
     } catch (error) {
         console.error('Error:', error);
+        descriptionElement.textContent = 'Error fetching description. Please try again.';
     }
 });
