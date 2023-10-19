1219a2011879dd5b7c0ac6e63b89b8ba0374dfed
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-19 13:16:07 -0500

    removed_lambda

--- netlify/functions/fetchGemData.js
+++ netlify/functions/fetchGemData.js
@@ -17,6 +17,7 @@ exports.handler = async (event) => {
     let browser;
 
     try {
+        console.log('Launching browser...');
         browser = await chromium.puppeteer.launch({
             args: chromium.args,
             defaultViewport: chromium.defaultViewport,
@@ -26,15 +27,21 @@ exports.handler = async (event) => {
 
         const page = await browser.newPage();
 
+        console.log('Navigating to GIA website...');
         await page.goto('https://www.gia.edu/report-check-landing');
 
+        console.log('Typing report number...');
         await page.type('#reportno', reportNumber);
 
+        console.log('Clicking the lookup button...');
         await page.click('.btn.search-btn[data-uw-rm-form="submit"]');
 
-        await page.waitForTimeout(5000);  // wait for 5 seconds for data to load
+        console.log('Waiting for data to load...');
+        await page.waitForTimeout(5000);
 
+        console.log('Trying to extract data...');
         const reportData = await page.$eval('#SHAPE', el => el.textContent);
+        console.log('Data extracted:', reportData);
 
         await browser.close();
 
@@ -60,20 +67,4 @@ exports.handler = async (event) => {
             },
         };
     }
-
-console.log('Navigating to GIA website...');
-await page.goto('https://www.gia.edu/report-check-landing');
-
-console.log('Typing report number...');
-await page.type('#reportno', reportNumber);
-
-console.log('Clicking the lookup button...');
-await page.click('.btn.search-btn[data-uw-rm-form="submit"]');
-
-console.log('Waiting for data to load...');
-await page.waitForTimeout(10000);
-
-console.log('Trying to extract data...');
-const reportData = await page.$eval('#SHAPE', el => el.textContent);
-console.log('Data extracted:', reportData);
 };
