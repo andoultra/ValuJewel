4b1e45630a3264e6f308cfbbbec5121f76465258
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-19 06:43:28 -0500

    undo

--- netlify/functions/generate-description.js
+++ netlify/functions/generate-description.js
@@ -4,23 +4,21 @@ const apiKey = process.env.OPENAI_API_KEY;
 const maxExecutionTime = 8000; // Set a maximum execution time (in milliseconds)
 
 async function describeJewelry(type, material, cut, ringSize, necklaceLength) {
-    let promptText;
+    let promptDescription = `Generate a technical description of a ${type} jewelry piece made of ${material}. Include details such as design, size, and any unique features.`;
 
-    if (type === 'ring') {
-        promptText = `Generate a technical description of a ${type} jewelry piece made of ${material} with a size of ${ringSize}. Include details such as design, cut, size, and any unique features.`;
-    } else if (type === 'necklace') {
-        promptText = `Generate a technical description of a ${type} jewelry piece made of ${material} with a length of ${necklaceLength}. Include details such as design, cut, size, and any unique features.`;
-    } else {
-        promptText = `Generate a technical description of a ${type} jewelry piece made of ${material}. Include details such as design, cut, size, and any unique features.`;
+    if (type === 'ring' && ringSize) {
+        promptDescription += ` The ring size is ${ringSize}.`;
+    }
+
+    if (type === 'necklace' && necklaceLength) {
+        promptDescription += ` The necklace length is ${necklaceLength}.`;
     }
 
     try {
-        const startTime = Date.now();
-
         const response = await axios.post(
             'https://api.openai.com/v1/engines/text-davinci-003/completions',
             {
-                prompt: promptText,
+                prompt: promptDescription,
                 max_tokens: 150,
                 temperature: 0.2,
             },
@@ -32,18 +30,8 @@ async function describeJewelry(type, material, cut, ringSize, necklaceLength) {
             }
         );
 
-        const description = response.data.choices[0].text.trim();
+        return response.data.choices[0].text.trim();
 
-        // Calculate the time taken by the function
-        const executionTime = Date.now() - startTime;
-
-        // Check if execution time exceeds Lambda timeout
-        if (executionTime >= maxExecutionTime) {
-            console.warn('Function execution approaching Lambda timeout.');
-            throw new Error('Function execution time exceeded.');
-        }
-
-        return description;
     } catch (error) {
         console.error('Error:', error.response ? error.response.data : error.message);
         throw new Error('Failed to fetch description from OpenAI API');
@@ -52,10 +40,10 @@ async function describeJewelry(type, material, cut, ringSize, necklaceLength) {
 
 exports.handler = async (event) => {
     try {
-        const { type, material, Cut, ringSize, necklaceLength } = JSON.parse(event.body);
+        const { type, material, Cut, ringSize, necklaceLength } = event.queryStringParameters || {};
+
         const description = await describeJewelry(type, material, Cut, ringSize, necklaceLength);
 
-        // Configure CORS headers
         const headers = {
             'Access-Control-Allow-Origin': 'https://andoultra.github.io',
             'Access-Control-Allow-Headers': 'Content-Type',
@@ -66,8 +54,10 @@ exports.handler = async (event) => {
             headers,
             body: JSON.stringify({ description }),
         };
+
     } catch (error) {
         console.error('Error:', error);
+
         return {
             statusCode: 500,
             headers: {
