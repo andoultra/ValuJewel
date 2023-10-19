796ed618111f1de71755f6204f2c6c58b8d0c601
AndoUltra <andrewhcalhoun98@gmail.com>
2023-10-19 13:08:52 -0500

    new_package

--- netlify/functions/fetchGemData.js
+++ netlify/functions/fetchGemData.js
@@ -1,4 +1,4 @@
-const chrome = require('chrome-aws-lambda');
+const chromium = require('chrome-aws-lambda');
 
 exports.handler = async (event) => {
     const reportNumber = event.queryStringParameters.reportNumber;
@@ -7,22 +7,32 @@ exports.handler = async (event) => {
         return {
             statusCode: 400,
             body: JSON.stringify({ error: 'Report number is required' }),
+            headers: {
+                'Access-Control-Allow-Origin': 'https://andoultra.github.io',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            },
         };
     }
 
-    let browser = null;
+    let browser;
+
     try {
-        browser = await chrome.puppeteer.launch({
-            args: chrome.args,
-            executablePath: await chrome.executablePath,
-            headless: chrome.headless,
+        browser = await chromium.puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless,
         });
-        
+
         const page = await browser.newPage();
+
         await page.goto('https://www.gia.edu/report-check-landing');
+
         await page.type('#reportno', reportNumber);
+
         await page.click('.btn.search-btn[data-uw-rm-form="submit"]');
-        await page.waitForTimeout(5000); // increased to 5 seconds
+
+        await page.waitForTimeout(5000);  // wait for 5 seconds for data to load
 
         const reportData = await page.$eval('#SHAPE', el => el.textContent);
 
@@ -30,22 +40,25 @@ exports.handler = async (event) => {
 
         return {
             statusCode: 200,
+            body: JSON.stringify({ data: reportData }),
             headers: {
                 'Access-Control-Allow-Origin': 'https://andoultra.github.io',
                 'Access-Control-Allow-Headers': 'Content-Type'
             },
-            body: JSON.stringify({ data: reportData }),
         };
 
     } catch (error) {
+        if (browser) {
+            browser.close();
+        }
         return {
             statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to retrieve data: ' + error.message }),
             headers: {
                 'Access-Control-Allow-Origin': 'https://andoultra.github.io',
                 'Access-Control-Allow-Headers': 'Content-Type'
             },
-            body: JSON.stringify({ error: `Failed to retrieve data: ${error.message}` }) };
-            
+        };
     }
 
 console.log('Navigating to GIA website...');
